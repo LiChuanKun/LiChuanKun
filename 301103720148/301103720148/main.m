ee4aea86691232c860ef7ba9fb0f1fdf051c594c
lanou3g <934657154@qq.com>
2015-07-13 15:14:39 +0800

    WWW Code <2015.07.13>

---
+++ 301103720148/301103720148/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  301103720148
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 李传昆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
